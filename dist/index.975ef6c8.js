0561371b3ce0d649a5e5de6b3a8304fcd510d028
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-05-04 22:19:31 +1000

    added styling to the web

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -562,12 +562,17 @@ new Chart(ctx, {
     type: "line",
     data: {
         labels: [
-            "Red",
-            "Blue",
-            "Yellow",
-            "Green",
-            "Purple",
-            "Orange"
+            "Jan",
+            "Feb",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "Aug",
+            "Sep",
+            "Nov",
+            "Dec"
         ],
         datasets: [
             {
@@ -578,23 +583,54 @@ new Chart(ctx, {
                     3,
                     5,
                     2,
-                    3
+                    3,
+                    11,
+                    4,
+                    7,
+                    9,
+                    10,
+                    5
                 ],
-                borderWidth: 1,
-                backgroundColor: [
-                    "red",
-                    "blue",
-                    "yellow",
-                    "purple",
-                    "orange"
-                ]
+                borderWidth: 1
             }
         ]
     },
     options: {
+        animations: {
+            tension: {
+                duration: 1000,
+                easing: "linear",
+                from: 1,
+                to: 0,
+                loop: true
+            }
+        },
         scales: {
             y: {
+                title: {
+                    display: true,
+                    text: "Price in AUD",
+                    font: {
+                        style: "italic",
+                        weight: "bold"
+                    }
+                },
                 beginAtZero: true
+            }
+        },
+        plugins: {
+            // add chart title
+            title: {
+                display: true,
+                text: "Price Chart",
+                font: {
+                    size: 25,
+                    weight: "bold"
+                }
+            },
+            // hide dataset label
+            legend: {
+                display: false
             }
         }
     }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,66 @@
 const ctx = document.getElementById("myChart");
 
 new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "Price",
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1,
-        backgroundColor: ["red", "blue", "yellow", "purple", "orange"],
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
+	type: "line",
+	data: {
+		labels: [
+			"Jan",
+			"Feb",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"Aug",
+			"Sep",
+			"Nov",
+			"Dec",
+		],
+		datasets: [
+			{
+				label: "Price",
+				data: [12, 19, 3, 5, 2, 3, 11, 4, 7, 9, 10, 5],
+				borderWidth: 1,
+			},
+		],
+	},
+	options: {
+		animations: {
+			tension: {
+				duration: 1000,
+				easing: "linear",
+				from: 1,
+				to: 0,
+				loop: true,
+			},
+		},
+		scales: {
+			y: {
+				title: {
+					display: true,
+					text: "Price in AUD",
+					font: {
+						style: "italic",
+						weight: "bold",
+					},
+				},
+				beginAtZero: true,
+			},
+		},
+		plugins: {
+			// add chart title
+			title: {
+				display: true,
+				text: "Price Chart",
+				font: {
+					size: 25,
+					weight: "bold",
+				},
+			},
+			// hide dataset label
+			legend: {
+				display: false,
+			},
+		},
+	},
 });
