0775d1dc2c7634e54ca52120c595b903e438c8be
Toe Aung Yint <ToeAung.Yint@monash.edu>
2023-05-08 14:48:57 +1000

    added functionality to display chart

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -557,76 +557,67 @@ function hmrAccept(bundle, id) {
 }
 
 },{}],"8lqZg":[function(require,module,exports) {
-const ctx = document.getElementById("myChart");
-new Chart(ctx, {
-    type: "line",
-    data: {
-        labels: [
-            "Jan",
-            "Feb",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "Aug",
-            "Sept",
-            "Nov",
-            "Dec"
-        ],
-        datasets: [
-            {
-                label: "Price",
-                data: [
-                    12,
-                    19,
-                    3,
-                    5,
-                    2,
-                    3,
-                    11,
-                    4,
-                    7,
-                    9,
-                    10,
-                    5
-                ],
-                borderWidth: 1
-            }
-        ]
-    },
-    options: {
-        animations: {
-            tension: {
-                duration: 1000,
-                easing: "linear",
-                from: 1,
-                to: 0,
-                loop: true
-            }
-        },
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        },
-        plugins: {
-            // add chart title
-            title: {
-                display: true,
-                text: "Price Chart for the year 2023",
-                font: {
-                    size: 16,
-                    weight: "bold"
-                }
-            },
-            // hide dataset label
-            legend: {
-                display: false
-            }
-        }
-    }
-});
+const year = document.querySelector("#startYear");
+document.querySelector(".calculate").addEventListener("click", (e)=>{
+    e.preventDefault();
+    console.log(year.value);
+}); // const ctx = document.getElementById("myChart");
+ // new Chart(ctx, {
+ //   type: "line",
+ //   data: {
+ //     labels: [
+ //       "Jan",
+ //       "Feb",
+ //       "March",
+ //       "April",
+ //       "May",
+ //       "June",
+ //       "July",
+ //       "Aug",
+ //       "Sept",
+ //       "Nov",
+ //       "Dec",
+ //     ],
+ //     datasets: [
+ //       {
+ //         label: "Price",
+ //         data: [12, 19, 3, 5, 2, 3, 11, 4, 7, 9, 10, 5],
+ //         borderWidth: 1,
+ //       },
+ //     ],
+ //   },
+ //   options: {
+ //     animations: {
+ //       tension: {
+ //         duration: 1000,
+ //         easing: "linear",
+ //         from: 1,
+ //         to: 0,
+ //         loop: true,
+ //       },
+ //     },
+ //     scales: {
+ //       y: {
+ //         beginAtZero: true,
+ //       },
+ //     },
+ //     plugins: {
+ //       // add chart title
+ //       title: {
+ //         display: true,
+ //         text: "Price Chart for the year 2023",
+ //         font: {
+ //           size: 16,
+ //           weight: "bold",
+ //         },
+ //       },
+ //       // hide dataset label
+ //       legend: {
+ //         display: false,
+ //       },
+ //     },
+ //   },
+ // });
 
 },{}]},["jC2qd","8lqZg"], "8lqZg", "parcelRequire94c2")
 
--- src/index.js
+++ src/index.js
@@ -1,58 +1,94 @@
-const ctx = document.getElementById("myChart");
+const year = document.querySelector("#startYear");
+const startMonth = document.querySelector("#startMonth");
+const endMonth = document.querySelector("#endMonth");
+const retailPrice = document.querySelector("#retailPrice");
+const financialCrisis = document.querySelector("#yes-btn");
+const form = document.querySelector(".form");
+const formSection = document.querySelector(".form-section");
+const chartSection = document.querySelector(".chart-section");
+const priceChart = document.querySelector("#priceChart");
+const newCalculation = document.querySelector(".new-calculation");
+let lineChart;
+const labels = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Nov",
+  "Dec",
+];
 
-new Chart(ctx, {
-	type: "line",
-	data: {
-		labels: [
-			"Jan",
-			"Feb",
-			"March",
-			"April",
-			"May",
-			"June",
-			"July",
-			"Aug",
-			"Sept",
-			"Nov",
-			"Dec",
-		],
-		datasets: [
-			{
-				label: "Price",
-				data: [12, 19, 3, 5, 2, 3, 11, 4, 7, 9, 10, 5],
-				borderWidth: 1,
-			},
-		],
-	},
-	options: {
-		animations: {
-			tension: {
-				duration: 1000,
-				easing: "linear",
-				from: 1,
-				to: 0,
-				loop: true,
-			},
-		},
-		scales: {
-			y: {
-				beginAtZero: true,
-			},
-		},
-		plugins: {
-			// add chart title
-			title: {
-				display: true,
-				text: "Price Chart for the year 2023",
-				font: {
-					size: 16,
-					weight: "bold",
-				},
-			},
-			// hide dataset label
-			legend: {
-				display: false,
-			},
-		},
-	},
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let prices = [
+    Number.parseInt(retailPrice.value),
+    Number.parseInt(retailPrice.value),
+    Number.parseInt(retailPrice.value),
+    Number.parseInt(retailPrice.value),
+    Number.parseInt(retailPrice.value),
+    Number.parseInt(retailPrice.value),
+    Number.parseInt(retailPrice.value),
+    Number.parseInt(retailPrice.value),
+    Number.parseInt(retailPrice.value),
+    Number.parseInt(retailPrice.value),
+    Number.parseInt(retailPrice.value),
+    Number.parseInt(retailPrice.value),
+  ];
+  for (let i = 0; i < prices.length; i++) {
+    if (financialCrisis.checked) {
+      prices[i] = prices[i] + prices[i] * (10 / 100);
+    }
+    if (i >= 7 && i <= 11) {
+      prices[i] = prices[i] + prices[i] * (5 / 100);
+    }
+  }
+  console.log(prices);
+
+  formSection.classList.add("hidden");
+  chartSection.classList.remove("hidden");
+  lineChart = new Chart(priceChart, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Unit price",
+          data: prices,
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: `Price Chart for the year ${year.value}`,
+          font: {
+            size: 16,
+            weight: "bold",
+          },
+        },
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+
+  year.value = "";
+  startMonth.value = "";
+  endMonth.value = "";
+  retailPrice.value = "";
+  financialCrisis.checked = false;
+});
+
+newCalculation.addEventListener("click", () => {
+  formSection.classList.remove("hidden");
+  lineChart.destroy();
+  chartSection.classList.add("hidden");
 });
