3913e24d20fa6dc83c74302f12fcd6d8892e9a79
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-07-11 22:59:41 +1000

    rewrite the app using vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
-	<h1>Hello World</h1>
+	<div class="container">
+		<h1>smart-pricing</h1>
+		<p>
+			<strong>Built using: </strong>Vue (Options API), HTML, CSS, JavaScript,
+			Firebase, Snackbar.js, Vite
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris congue
+			ipsum eleifend tempus eleifend. Nunc eu ante scelerisque, tincidunt leo
+			in, ullamcorper nisl. Nulla at consequat urna. Orci varius natoque
+			penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+			Curabitur venenatis tellus diam. Etiam lobortis et metus et interdum. Sed
+			tincidunt risus id lacus mollis, sed scelerisque metus imperdiet. Proin
+			est ex, lobortis vitae mauris quis, volutpat aliquam diam. Pellentesque
+			porta ex ut scelerisque posuere. Vivamus a urna diam. Sed in lectus est.
+			Vestibulum dignissim dignissim tellus vel consequat.
+		</p>
+	</div>
 </template>
 
 <script setup></script>
 
-<style></style>
+<style scoped>
+h1 {
+	text-align: center;
+	margin-bottom: 50px;
+	text-transform: uppercase;
+}
+.container {
+	max-width: 1000px;
+	margin: 100px auto;
+	padding: 20px;
+}
+</style>
