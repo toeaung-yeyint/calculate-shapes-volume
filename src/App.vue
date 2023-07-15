134b0720ff64b8ffa928d7938c788dee7bbb4d15
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-07-16 00:06:51 +1000

    added search functionality

--- src/App.vue
+++ src/App.vue
@@ -18,43 +18,13 @@
 			porta ex ut scelerisque posuere. Vivamus a urna diam. Sed in lectus est.
 			Vestibulum dignissim dignissim tellus vel consequat.
 		</p>
-		<SearchBar />
+		<SearchBar @inputProvided="savedSearchTeam" />
 		<CardContainer>
 			<Card
-				image="https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/cone.jpg?alt=media&token=ff8f9669-3bb5-4480-b228-b2e82d817175"
-				label="Cone"
-			/>
-			<Card
-				image="https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/cube.jpg?alt=media&token=7da00abc-4ca4-45ba-92ca-3b3f4403194d"
-				label="Cube"
-			/>
-			<Card
-				image="https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/cuboid.jpg?alt=media&token=b7e98bb1-5e63-4c23-b315-c4f5210e756c"
-				label="Cuboid"
-			/>
-			<Card
-				image="https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/cylinder.jpg?alt=media&token=a0353329-fc3c-4c28-844b-d5b526e1ca0d"
-				label="Cylinder"
-			/>
-			<Card
-				image="https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/hexagonal%20prism.jpg?alt=media&token=12d18ba3-df94-44e4-b9ac-a06490fca9cd"
-				label="Hexagonal Prism"
-			/>
-			<Card
-				image="https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/sphere.jpg?alt=media&token=8b01c5f8-c815-481f-8eb2-b58828aa35b2"
-				label="Sphere"
-			/>
-			<Card
-				image="https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/square-based%20pyramid.jpg?alt=media&token=57822014-c13b-4116-a058-f8fadab26d33"
-				label="Square-based Pyramid"
-			/>
-			<Card
-				image="https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/triangle-based%20pyramid.jpg?alt=media&token=7e452b67-9012-4176-b30f-d4cfe39b7fdd"
-				label="Triangle-based Pyramid"
-			/>
-			<Card
-				image="https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/triangle-based%20pyramid.jpg?alt=media&token=7e452b67-9012-4176-b30f-d4cfe39b7fdd"
-				label="Triangular Prism"
+				v-for="(shape, index) in filteredShape"
+				:key="index"
+				:label="shape.label"
+				:image="shape.image"
 			/>
 		</CardContainer>
 	</div>
@@ -64,6 +34,67 @@
 import CardContainer from "./components/CardContainer.vue";
 import Card from "./components/Card.vue";
 import SearchBar from "./components/SearchBar.vue";
+import { ref, computed } from "vue";
+
+const shapes = ref([
+	{
+		label: "Cone",
+		image:
+			"https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/cone.jpg?alt=media&token=ff8f9669-3bb5-4480-b228-b2e82d817175",
+	},
+	{
+		label: "Cube",
+		image:
+			"https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/cube.jpg?alt=media&token=7da00abc-4ca4-45ba-92ca-3b3f4403194d",
+	},
+	{
+		label: "Cuboid",
+		image:
+			"https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/cuboid.jpg?alt=media&token=b7e98bb1-5e63-4c23-b315-c4f5210e756c",
+	},
+	{
+		label: "Cylinder",
+		image:
+			"https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/cylinder.jpg?alt=media&token=a0353329-fc3c-4c28-844b-d5b526e1ca0d",
+	},
+	{
+		label: "Hexagonal Prism",
+		image:
+			"https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/hexagonal%20prism.jpg?alt=media&token=12d18ba3-df94-44e4-b9ac-a06490fca9cd",
+	},
+	{
+		label: "Sphere",
+		image:
+			"https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/sphere.jpg?alt=media&token=8b01c5f8-c815-481f-8eb2-b58828aa35b2",
+	},
+	{
+		label: "Square-based Pyramid",
+		image:
+			"https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/square-based%20pyramid.jpg?alt=media&token=57822014-c13b-4116-a058-f8fadab26d33",
+	},
+	{
+		label: "Triangle-based Pyramid",
+		image:
+			"https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/triangle-based%20pyramid.jpg?alt=media&token=7e452b67-9012-4176-b30f-d4cfe39b7fdd",
+	},
+	{
+		label: "Triangular Prism",
+		image:
+			"https://firebasestorage.googleapis.com/v0/b/shapes-97497.appspot.com/o/triangular%20prism.jpg?alt=media&token=3ab77eb9-827a-4834-84c4-f6054bca1399",
+	},
+]);
+
+const searchShape = ref("");
+
+const savedSearchTeam = (payload) => {
+	searchShape.value = payload;
+};
+
+const filteredShape = computed(() => {
+	return shapes.value.filter((shape) => {
+		return shape.label.toLowerCase().includes(searchShape.value.toLowerCase());
+	});
+});
 </script>
 
 <style scoped></style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,14 +1,19 @@
 <template>
 	<form
+		@input="$emit('inputProvided', (payload = searchShape))"
 		class="flex items-center border-2 mt-20 mb-12 focus-within:border-black"
 	>
 		<input
 			class="p-2 flex-grow focus:outline-none"
 			type="text"
 			placeholder="Search for your 3D shape..."
+			v-model="searchShape"
 		/>
 		<i class="fa-solid fa-magnifying-glass text-gray-400 px-3"></i>
 	</form>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const searchShape = ref("");
+</script>
