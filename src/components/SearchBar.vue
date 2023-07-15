58d6ecae281bda959c8a0805e11e4dc2b4313180
Toe Aung Ye Yint <toeaungyeyint@gmail.com>
2023-07-15 22:25:32 +1000

    added search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,9 +1,14 @@
 <template>
-	<input
-		class="w-full border-2 mt-20 mb-8 p-2"
-		type="text"
-		placeholder="Search for your 3D shape..."
-	/>
+	<form
+		class="flex items-center border-2 mt-20 mb-12 focus-within:border-black"
+	>
+		<input
+			class="p-2 flex-grow focus:outline-none"
+			type="text"
+			placeholder="Search for your 3D shape..."
+		/>
+		<i class="fa-solid fa-magnifying-glass text-gray-400 px-3"></i>
+	</form>
 </template>
 
 <script setup></script>
