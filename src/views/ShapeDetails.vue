a1424a95489f1074c2e82bc0422f2949df8da96e
Toe Aung Yint <ToeAung.Yint@monash.edu>
2023-07-18 14:59:44 +1000

    added view for shape details page

--- src/views/ShapeDetails.vue
+++ src/views/ShapeDetails.vue
@@ -1,29 +1,45 @@
 <template>
   <IntroSection>
-    <div class="mt-20 flex items-center gap-x-8">
-      <div class="w-5/12">
-        <RouterLink to="/">
-          <button class="w-full mb-4 p-1 rounded bg-gray-200 hover:bg-gray-300">
-            Back to menu
-          </button>
-        </RouterLink>
-        <img :src="shape.image" alt="" class="border-2 w-full" />
+    <RouterLink to="/">
+      <button
+        class="mt-20 w-full mb-12 py-2 rounded bg-gray-200 hover:bg-gray-300"
+      >
+        Back to menu
+      </button>
+    </RouterLink>
+    <div class="flex items-center gap-x-8 h-80">
+      <div class="w-1/2 h-full">
+        <img
+          :src="shape.image"
+          alt=""
+          class="border-2 w-full h-full object-contain"
+        />
       </div>
-      <div class="w-7/12 flex flex-col max-w-xs mx-auto">
+      <div class="w-1/2 flex flex-col max-w-xs mx-auto">
         <h1 class="font-bold text-xl text-center mb-4">
           Volume of a {{ shape.label }}
         </h1>
         <div
           v-for="(property, index) in shape.properties"
           :key="index"
-          class="flex flex-col"
+          class="flex items-center mb-4"
         >
-          <label :for="property">{{ property }}:</label>
+          <label :for="property" class="w-3/12">{{ property }}:</label>
           <input
             :id="property"
-            type="text"
-            class="border-2 flex-grow mt-2 my-4"
+            type="number"
+            class="border-2 flex-grow w-9/12"
           />
+        </div>
+        <div class="flex items-center mb-4">
+          <label for="unit" class="w-3/12">Unit:</label>
+          <select class="border-2" name="unit" id="unit">
+            <option value="">-</option>
+            <option value="in">in³</option>
+            <option value="ft">ft³</option>
+            <option value="cm">cm³</option>
+            <option value="m">m³</option>
+          </select>
         </div>
         <button
           class="w-full bg-blue-500 mb-4 p-1 text-white rounded hover:bg-blue-600"
@@ -39,8 +55,8 @@
 import { ref, onMounted } from "vue";
 import IntroSection from "../components/IntroSection.vue";
 
-import cone from "../assets/cone.jpg";
-import cube from "../assets/cube.jpg";
+import cone from "../assets/cone-details.jpg";
+import cube from "../assets/cube-details.jpg";
 import cuboid from "../assets/cuboid.jpg";
 import cylinder from "../assets/cylinder.jpg";
 import hexPrism from "../assets/hexagonal-prism.jpg";
@@ -56,19 +72,18 @@ const shapes = ref([
     id: 1,
     label: "Cone",
     image: cone,
-    properties: ["Radius", "Height"],
+    properties: ["Height(h)", "Radius(r)"],
   },
   {
     id: 2,
     label: "Cube",
     image: cube,
-    properties: ["Edge"],
+    properties: ["Edge(a)"],
   },
   {
     id: 3,
     label: "Cuboid",
     image: cuboid,
-    properties: ["Length", "Width", "Height"],
   },
   {
     id: 4,
