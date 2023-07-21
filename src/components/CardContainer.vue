4cbf996511e2baab10804ddff17cfbb57c140bf5
Toe Aung Yint <ToeAung.Yint@monash.edu>
2023-07-21 14:46:08 +1000

    production version of the app

--- src/components/CardContainer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div
-		class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 justify-items-center gap-x-5 gap-y-10"
-	>
-		<slot></slot>
-	</div>
-</template>
-
-<script setup></script>
